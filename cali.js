0de7630284428370aa50f0fbd2f99fe1847ce0a3
abdiqaadir mahamuud hasssan <112267536+abdiqaadirmahamuud@users.noreply.github.com>
2022-09-20 11:18:04 -0700

    waa java

---
+++ cali.js
@@ -0,0 +1,62 @@
+ /*
+ console.log('hellow java script');
+ console.log(" stiil explore js");
+
+ //variables
+
+ let name = 'mohamed';
+
+ console.log("hi"+name );
+
+ let x=10;
+ let y=5;
+ let z=x+y;        //addition
+ console.log(z);
+
+ x='shaah';
+ console.log('x is'+x)
+*/
+ // conditions > cantrol flow 
+ let personalNmae='ahmed ali'
+ if (personalNmae=== 'ashia'){
+  console.log('hellow mr '+personalNmae)
+}else{
+console.log('hellow msr ashia'+name)
+}
+
+let x=10
+if (x===0){
+
+  console.log ('x is gether than 0');
+} else if ( x===1){
+
+  console.log('x is one');
+} else if (x==2){
+  console.log('x is two');
+}else{
+
+  console.log('i do not know');
+}
+
+// compond condition
+let today= 'jimce';
+
+if (today==='khamiis' || today=== 'jimce'){
+  console.log('happy weekend');
+
+} else {
+
+  console.log('week education!!!');
+}
+// prime numbers 37 11 13 17 19 23
+
+let prime =10;
+if (prime % 2=== 0  || prime % 3 === 0){
+  console.log( prime +' is not prime number');
+}
+
+
+
+
+
+
